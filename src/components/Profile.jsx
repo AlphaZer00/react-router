23759e726b5c415d29ea3dd6ea89fec9083df004
AlphaZer00 <batudasdemir@gmail.com>
2025-04-27 09:50:14 -0400

    Fix typo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Profile = () => {
     return (
         <div>
-            <h1>Hello from thr profile page!</h1>
+            <h1>Hello from the profile page!</h1>
             <p>How are you?</p>
             <hr />
             <h2>The profile visited is here:</h2>
