d4ea4f77f2e18e6114b21ff9e26c500bfad67b4c
AlphaZer00 <batudasdemir@gmail.com>
2025-04-27 10:06:28 -0400

    Refactor route array to its own file

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Profile from "./components/Profile.jsx";
-import Popeye from "./components/Popeye.jsx";
-import Spinach from "./components/Spinach.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
+import routes from "./routes.jsx";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-    {
-        path: "/profile",
-        element: <Profile />,
-        children: [
-            { path: "spinach", element: <Spinach /> },
-            { path: "popeye", element: <Popeye /> },
-        ],
-    },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
