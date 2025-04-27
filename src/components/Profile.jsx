c55f37dc4099a046410bff436e2960c14b41f2b0
AlphaZer00 <batudasdemir@gmail.com>
2025-04-27 09:46:28 -0400

    Add Outlet

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+
 const Profile = () => {
     return (
         <div>
             <h1>Hello from thr profile page!</h1>
             <p>How are you?</p>
+            <hr />
+            <h2>The profile visited is here:</h2>
+            <Outlet />
         </div>
     );
 };
