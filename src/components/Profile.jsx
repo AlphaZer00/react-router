eded59ad4e1d2a7f58396c110f421ec02319c60c
AlphaZer00 <batudasdemir@gmail.com>
2025-04-27 09:02:45 -0400

    Create folder

---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+    return (
+        <div>
+            <h1>Hello from thr profile page!</h1>
+            <p>How are you?</p>
+        </div>
+    );
+};
+
+export default Profile;
